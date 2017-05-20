ecb0f2219f5f8b018228d17858287449f43ef455
Bram Willemse <contact@bramwillemse.nl>
2017-05-20 19:37:59 +0200

    Add a bit more dummy data, style base layout / typography

--- src/scripts/components/notes.js
+++ src/scripts/components/notes.js
@@ -5,12 +5,16 @@ export default {
     name: 'notes',
 
     template: `
-        <div class="notes">
-            <div v-for="note in notes" class="note">
-                <p>{{note.text}}</p>
-                <p>Location: {{note.location.lat}}, {{note.location.lon}}
-            </div>
-        </div>
+        <section class="notes">
+            <article v-for="note in notes" class="note">
+                <header>
+                    <h1 class="h3 note__title">{{note.title}}</h1>
+                </header>
+                <div class="note__meta note__location">{{note.location.city}}, {{note.location.country}}</div>
+
+                <p class="note__text">{{note.text}}</p>
+            </article>
+        </section>
     `,
 
     data() { 
